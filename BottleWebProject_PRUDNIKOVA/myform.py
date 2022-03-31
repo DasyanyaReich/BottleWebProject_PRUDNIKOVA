a22a1cb44eb44d5429ad0693f3355172f6c1369d
Dasyanya <2003pds@mail.ru>
2022-03-31 09:13:25 +0300

    Dop

--- BottleWebProject_PRUDNIKOVA/myform.py
+++ BottleWebProject_PRUDNIKOVA/myform.py
@@ -1,4 +1,5 @@
 #Подключение методов для отправки HTTP запросов
+import pdb
 from bottle import post, request
 
 
@@ -6,9 +7,9 @@ from bottle import post, request
 def my_form():
     #присвоение переменной значение поля адреса
     mail = request.forms.get('ADRESS')
-    #text = request.forms.get('QUEST')
+    text = request.forms.get('QUEST')
     #regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-    if mail:
-        return "Thanks! The answer will be sent to the mail %s " % mail
-   
+    questions = {mail: text}
+    pdb.set_trace()
 
+    return "Thanks! The answer will be sent to the mail %s " % mail
